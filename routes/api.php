b945c64df5e82374dadc271baa8b7b12ea88324e
Ikaro C. Laborda <iclaborda@msn.com>
2024-12-12 13:02:53 +0000

    fix: adding missing auth routes

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\Auth\LogoutController;
+use App\Http\Controllers\Api\Auth\RefreshController;
 use App\Http\Controllers\Api\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
     Route::post('/register', RegisterController::class);
+    Route::post('/login', LoginController::class);
+    Route::post('/logout', LogoutController::class);
+    Route::post('/refresh', RefreshController::class);
 });
